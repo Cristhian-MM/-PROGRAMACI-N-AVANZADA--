a4a52e3a0364eda137e499db5eab4c62b4883590
CristhianMM <moralesmendezcristhian@gmail.com>
2021-11-13 23:33:33 -0600

    Clase persona

--- mainU4.py
+++ mainU4.py
@@ -8,3 +8,16 @@ Veronica = E.Administrador('Veronica','Gamino', 39, 1.48, 'Licenciatura', 231101
 Juan = E.Contador('Juan','Reyes',43,1.65,'Licenciatura',2311297057, 'M&M Company', 4000)
 Manuel = E.JefeDeMarketing('Manuel','Torres',25,1.88,'Licenciatura',2314782905,'M&M Company',2800)
 Cristhian = E.Gerente('Cristhian', 'Morales', 21,1.68, 'Licenciatura',2261111837,'M&M Company',6000)
+
+print('\n--Clase persona--')
+Diego.Presentarce()
+Diego.Vestir('Pantalon negro','Playera Blanca','Gafas')
+Diego.Comer('Hamburguesa','Jugo de naranja')
+Diego.AccionAleatoria()
+Diego.Dormir(6)
+
+Camila.Presentarce()
+Camila.Vestir('Vestido Rojo', 'Saco negro', 'Chalina')
+Camila.Comer('Coctel de frutas','Frappe')
+Camila.AccionAleatoria()
+Camila.Dormir(9)
