b2b78e6d3397b8e1e1a9d1e6c1a839b6f6d14c71
CristhianMM <moralesmendezcristhian@gmail.com>
2021-11-13 23:21:27 -0600

    Clase Gerente

--- EvaluaciónUnidad4.py
+++ EvaluaciónUnidad4.py
@@ -86,3 +86,56 @@ class Empleado(Persona):
         print(' Empresa: ',Empleado.getEmpresa(self))
         print(' Puesto: ',Empleado.getPuesto(self))
         print('|--------------------------------|')
+
+class Gerente(Empleado):
+    _puesto = 'Gerente'
+    def __init__(self, nombre, apellido, edad, estatura, nivelAcademico, numeroTelefonico, empresa, sueldo):
+        super().__init__(nombre, apellido, edad, estatura, nivelAcademico, numeroTelefonico, empresa, self._puesto, sueldo)
+
+    def Entrevistar(self, gerente, persona):
+        print('\n--Entrevista de trabajo--')
+        print('**Entrevistador:')
+        print('Buen dia',persona.getNombre(), ',soy',gerente.getNombre(),'y sere la persona responsable de realizar la entrevista.')
+        print('La empresa',gerente.getEmpresa(),'le agradece su interes por formar parte de nuestro equipo de trabajo.')
+        print('**Solicitante:')
+        print('Un placer',gerente.getNombre(),', gracias por considerar mi solicitud')
+        print('**Entrevistador:')
+        print('Bien, comencemos con la entrevista.')
+        print('¿Cual es su nombre completo?')
+        print('**Solicitante:')
+        print(persona.getNombre(),persona.getApellido())
+        print('**Entrevistador:')
+        print('¿Cual es su edad?')
+        print('**Solicitante:')
+        print(persona.getEdad(),'años')
+        print('**Entrevistador:')
+        print('¿Cual es su maximo nivel academico?')
+        print('**Solicitante:')
+        print(persona.getNivelAcademico())
+        print('**Entrevistador:')
+        print('¿Cuenta con algun numero telefonico para futuro contacto?')
+        print('**Solicitante:')
+        print(persona.getNumeroTelefonico())
+        print('**Entrevistador:')
+        print('Eso es todo por el momento, en un futuro se le dara a conocer la resolucion de su solicitud.')
+
+    def Contratar(self, gerente, persona):
+        print('\n--Resolucion de Solicitud de Empleo--')
+        Decision = random.randint(1, 2)
+        if Decision == 1:
+            print('Aspirante:', persona.getNombre(), persona.getApellido())
+            print('Enhorabuena',persona.getNombre(),'su solicitud de empleo ha sido aprovada.')
+            print('Bienvenido a la empresa', gerente.getEmpresa())
+        elif Decision == 2:
+            print('Aspirante:',persona.getNombre(),persona.getApellido())
+            print('Sentimos informarles que su solicitud de empleo ha sido rechazada.')
+
+    def TrabajoAleatorio(self):
+        print(' ')
+        TA = random.randint(1, 3)
+        if TA == 1:
+            print('El Gerente',Persona.getNombre(self), 'esta planificando las actividaes de la empresa.')
+        elif TA == 2:
+            print('El Gerente',Persona.getNombre(self), 'esta organizando los recursos de la empresa.')
+        elif TA == 3:
+            print('El Gerente',Persona.getNombre(self), 'esta planificando los objetivos de la empresa.')
